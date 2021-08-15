0c3e4215493997cf8f3b607111f42da775632d93
rozsuimre <rozsuimre1@gmail.com>
2021-08-16 00:01:02 +0200

    PHP feltoltve

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<img src="programozo.jpg">
+<h1>Első PHP-s oldalam 2020-ban</h1>
+<h2>Nem baj</h2>
+<?php
+
+echo("Hello World ! Ezt a szöveget a PHP írta <br>");
+
+
+
+
+?>
+</body>
+</html>
